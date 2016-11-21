da8cbdd82a9b3f4ed06d5b2a1c1fd4fa3ef3f5d5
Charly Koza <cka@f4-group.com>
2016-11-21 17:07:44 +0100

    gmail.fr should get corrected to gmail.com

--- spec/mailcheckSpec.js
+++ spec/mailcheckSpec.js
@@ -117,6 +117,7 @@ describe("mailcheck", function() {
         expect(mailcheck.suggest('test@GNAIL.con', domains).domain).toEqual('gmail.com');
         expect(mailcheck.suggest('test@#gmail.com', domains).domain).toEqual('gmail.com');
         expect(mailcheck.suggest('test@comcast.nry', domains).domain).toEqual('comcast.net');
+        expect(mailcheck.suggest('test@gmail.fr', domains).domain).toEqual('gmail.com');
 
         expect(mailcheck.suggest('test@homail.con', domains, secondLevelDomains, topLevelDomains).domain).toEqual('hotmail.com');
         expect(mailcheck.suggest('test@hotmail.co', domains, secondLevelDomains, topLevelDomains).domain).toEqual('hotmail.com');
